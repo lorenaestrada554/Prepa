08ff07257a2b0111332f461e4ea42acbd715d196
lorenaestrada554 <55006924+lorenaestrada554@users.noreply.github.com>
2021-01-04 15:35:27 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,20 +1,22 @@
 <?php
 
-$hostname='ec2-18-210-51-239.compute-1.amazonaws.com';
-$database='db8k6qk08f7njf';
-$user='rlerwmudzipqma';
-$pass='9dd7af7373d2134c6134814b1355856a65bf5ed339f0e2df78b8ad5a974c1c62';
+$hostname='ec2-3-218-123-191.compute-1.amazonaws.com';
+$database='d9pr5aifgprbrk';
+$user='ftjmhxlmrknfjm';
+$pass='a950396198402283060bcb94c41148d68837731bdc234de9ffe3d2f6a19fb869';
 
-$conexion = pg_connect("host='ec2-18-210-51-239.compute-1.amazonaws.com' dbname=db8k6qk08f7njf port=5432 user=rlerwmudzipqma password=9dd7af7373d2134c6134814b1355856a65bf5ed339f0e2df78b8ad5a974c1c62") or die ("Error de Conexión".pg_last_error());
+$conexion = pg_connect("ec2-3-218-123-191.compute-1.amazonaws.com' dbname=d9pr5aifgprbrk port=5432 user=rlerwmudzipqma password=a950396198402283060bcb94c41148d68837731bdc234de9ffe3d2f6a19fb869") or die ("Error de Conexión".pg_last_error());
 
 
 $nombre = $_GET['nombre'];
-$apellido = $_GET['apellido'];
+$direccion = $_GET['direccion'];
 $email = $_GET['email'];
 $telefono = $_GET['telefono'];
 $contra = $_GET['contra'];
-$total = $nombre.", ".$apellido.", ".$email.", ".$telefono.", ".$contra."'";
-$querycont = pg_query($conexion, "INSERT INTO usuario(nombre,apellido,email,telefono,contra) VALUES (".$total.");");
+$estado = $_GET['estado'];
+$municipio = $_GET['municipio'];
+$total = $nombre.", ".$direccion.", ".$email.", ".$telefono.", ".$contra.",".$estado.", ".$municipio."'";
+$querycont = pg_query($conexion, "INSERT INTO usuario(nombre,apellido,email,telefono,contra,estado,municipio) VALUES (".$total.");");
 
 pg_close($conexion);
 
