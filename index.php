16a6faf44811e6e2eca0e0ae831aa70c5d66e745
lorenaestrada554 <55006924+lorenaestrada554@users.noreply.github.com>
2021-01-07 18:41:17 -0600

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ $database='d9pr5aifgprbrk';
 $user='ftjmhxlmrknfjm';
 $pass='a950396198402283060bcb94c41148d68837731bdc234de9ffe3d2f6a19fb869';
 
-$conexion = pg_connect("ec2-3-218-123-191.compute-1.amazonaws.com' dbname=d9pr5aifgprbrk port=5432 user=ftjmhxlmrknfjm password=a950396198402283060bcb94c41148d68837731bdc234de9ffe3d2f6a19fb869") or die ("Error de Conexión".pg_last_error());
+$conexion = pg_connect("host='ec2-3-218-123-191.compute-1.amazonaws.com' dbname=d9pr5aifgprbrk port=5432 user=ftjmhxlmrknfjm password=a950396198402283060bcb94c41148d68837731bdc234de9ffe3d2f6a19fb869") or die ("Error de Conexión".pg_last_error());
 
 
 $nombre = $_GET['nombre'];
