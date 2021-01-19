f7d564f1672bb01f42967a46d03369e08741b829
Slynnex <54971727+Slynnex@users.noreply.github.com>
2021-01-19 14:16:59 -0600

    Update estados.php

--- estados.php
+++ estados.php
@@ -7,7 +7,7 @@ $pass='a950396198402283060bcb94c41148d68837731bdc234de9ffe3d2f6a19fb869';
 
 $conexion = pg_connect("host='ec2-3-218-123-191.compute-1.amazonaws.com' dbname=d9pr5aifgprbrk port=5432 user=ftjmhxlmrknfjm password=a950396198402283060bcb94c41148d68837731bdc234de9ffe3d2f6a19fb869") or die ("Error de Conexión".pg_last_error());
 
-$consulta = "Select * from estados";
+$consulta = "Select * from estado";
 $resultado = pg_query($conexion, $consulta);
 $reporte = array();
 $cont =0;
