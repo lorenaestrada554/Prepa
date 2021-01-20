695741bfd124d5022eea291eea86f7a3104093db
CarolinaGutierrezS25 <58743673+CarolinaGutierrezS25@users.noreply.github.com>
2021-01-20 13:13:48 -0600

    cmt

--- estados.php
+++ estados.php
@@ -1,11 +1,7 @@
 <?php
-
-$hostname='ec2-3-218-123-191.compute-1.amazonaws.com';
-$database='d9pr5aifgprbrk';
-$user='ftjmhxlmrknfjm';
-$pass='a950396198402283060bcb94c41148d68837731bdc234de9ffe3d2f6a19fb869';
-
 $conexion = pg_connect("host='ec2-3-218-123-191.compute-1.amazonaws.com' dbname=d9pr5aifgprbrk port=5432 user=ftjmhxlmrknfjm password=a950396198402283060bcb94c41148d68837731bdc234de9ffe3d2f6a19fb869") or die ("Error de Conexión".pg_last_error());
+
+@pg_query($conexion, "SET NAMES 'utf8'");
 
 $consulta = "Select * from estado";
 $resultado = pg_query($conexion, $consulta);
